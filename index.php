993781ba153d579ae3f293edec8257324837a83e
mailcom <bonifacepromzy7@gmail.com>
2018-04-12 03:23:00 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="master.css">
+</head>
+<body>
+
+
+<div class="container">
+  <div class="center">
+    <p id="big">CURRENT TIME</p>
+    <p id="time"><?php echo date("h:i A");?></p>
+  </div>
+  </div>
+
+</body>
+</html>
